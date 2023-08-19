bc16b8b088919dc628ddaf8920db9dc41d51f381
rushikesh.c96 <rushikesh.c96@gmail.com>
2023-08-19 19:33:52 +0530

    portfolio done

--- project/src/app/portfolio/[category]/page.jsx
+++ project/src/app/portfolio/[category]/page.jsx
@@ -23,6 +23,36 @@ const Category = ({params}) => {
           />
         </div>
       </div>
+      <div className={styles.item}>
+        <div className={styles.content}>
+          <h1 className={styles.title}>Test</h1>
+          <p className={styles.desc}>desc</p>
+          <Button text="See More" url="#"/>
+        </div>
+        <div className={styles.imgContainer}>
+          <Image
+            className={styles.img}
+            fill={true}
+            src=""
+            alt=''
+          />
+        </div>
+      </div>
+      <div className={styles.item}>
+        <div className={styles.content}>
+          <h1 className={styles.title}>Test</h1>
+          <p className={styles.desc}>desc</p>
+          <Button text="See More" url="#"/>
+        </div>
+        <div className={styles.imgContainer}>
+          <Image
+            className={styles.img}
+            fill={true}
+            src=""
+            alt=''
+          />
+        </div>
+      </div>
     </div>
   )
 }
