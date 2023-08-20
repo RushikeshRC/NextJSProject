16b77f832ae784ee39d118773eb3cb6e3a9eab98
rushikesh.c96 <rushikesh.c96@gmail.com>
2023-08-20 10:31:18 +0530

    dark mode complete

---
+++ project/context/ThemeContext.js
@@ -0,0 +1,22 @@
+"use client"
+import { createContext, useState } from "react"
+
+   //if you are using context api it should be client side component
+   //Theme provider is a wrapper, same as layout
+
+export const ThemeContext = createContext()
+
+export const ThemeProvider = ({children}) =>{
+    const [mode, setMode] = useState("dark");
+
+    const toggle = ()=>{
+        setMode((prev) => (prev === "dark" ? "light" : "dark"));
+    };
+
+    return(<ThemeContext.Provider value={{toggle, mode}}>
+        <div className={`theme ${mode}`}>
+        {children}
+        </div>
+    </ThemeContext.Provider>
+    );
+};
